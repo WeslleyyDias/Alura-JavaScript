8bb182d58a1d0ebc8d3117df6eb0d5581c3de162
Weslleyydias <weslleyy3389@gmail.com>
2023-07-13 15:24:09 -0300

    codigos var e conversoes

---
+++ thuthy-falsy.js
@@ -0,0 +1,24 @@
+//  boolean
+const usuarioLogado = true
+const contapaga = false
+
+// truthy ou falsy
+
+// console.log(0 == false)
+// console.log(""==false)
+// console.log(1 ==false)
+
+// null = vazio ou nada
+// undefined = quando agente tem uma variavel declarada mais ela nao tem valor
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
