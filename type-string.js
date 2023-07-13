d9452f217111317b8ab4cb44ec1f3c75deb2c7fc
Weslleyydias <weslleyy3389@gmail.com>
2023-07-13 12:18:37 -0300

    Exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Ola, Mundo!";
+const texto2 = 'Ola, Mundo';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é";
+const meuNome = "Leonardo"
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string Ou template literal
